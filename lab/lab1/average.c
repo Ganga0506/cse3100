eceba4d5086a43d5ea66b856d7599be1edda5cbc
Ganga Nomika Mahesh <ganga.mahesh00@gmail.com>
2026-09-14 23:44:08 -0400

    added csft03 lab1

---
+++ lab/lab1/average.c
@@ -0,0 +1,20 @@
+// Only this line of comment is provided 
+
+#include <stdio.h>
+
+int main(void)
+{
+    double x;
+    double total = 0.0;
+    double average = 0.0;
+    int count = 0;
+
+    while (scanf("%lf", &x) == 1) {
+        total += x;
+        count++;
+        average = total / count;
+        printf("Total=%f Average=%f\n", total, average);
+    }
+    
+    return 0;
+}

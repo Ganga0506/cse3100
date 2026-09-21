4b3cb949032f0146b86d3528b9c18f0750a54d2f
Ganga Nomika Mahesh <ganga.mahesh00@gmail.com>
2026-09-20 23:12:26 -0400

    More assignments

---
+++ hw/hw1/pi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n, i;
+
+	printf("n = ");
+	scanf("%d", &n);
+
+	double pi = 0.;
+	//TODO
+	//add code below 
+
+	double power = 1.0;  
+
+	for (i = 0; i <= n; i++) {
+		double term = (4.0 / (8*i + 1))
+		            - (2.0 / (8*i + 4))
+		            - (1.0 / (8*i + 5))
+		            - (1.0 / (8*i + 6));
+		pi += term * power;
+		power /= 16.0;   
+	}
+
+
+	printf("PI = %.10f\n", pi);
+	return 0;
+}
